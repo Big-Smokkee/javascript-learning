91eca2853df2d90206940bcb00f6fa4e89f423c2
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-11 02:25:13 +0600

    java script light on off

--- index.js
+++
@@ -1,8 +0,0 @@
-/*Operator Preecedence*/
-// 1. Parenthesis ()
-// 2. Exponents
-// 3. multiplication & division & modulo 
-// 4. addtion & subtraction
-
-let result = 1 + 2 * 3+4 ** 2;
-console.log(result);
