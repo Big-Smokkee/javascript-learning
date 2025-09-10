870be76ba94525fe4c5a25d9e34bfd7cd7252694
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-11 00:52:08 +0600

    variables

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-// console.log(`hello`);
-// console.log(`I like pizza!`);
+// 1.declaration let x;
+// 2. assignment x = 10;
 
-// window.alert(`This is an alert`);
-// window.alert(`I like pizza!`);
-document.getElementById(`myh1`).textContent = `Hello`;
-document.getElementById(`myp`).textContent = `Faraz`;
+//1 way
+// let x;
+// x = 120;
+
+// console.log(x);
+
+//2nd way
+// let x = 120;
+// console.log(x);
+
+//3rd 
+let age = 25;
+let price = 10.99;
+let gpa = 3.89;
+let name='Far';
+let lastname = 'az'
+// console.log(gpa);
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price} in dollars`);
+// console.log(`Your gpa is ${gpa}`);
+// console.log(`Your name is ${name}${lastname}.`)
+
+// for seeing the datatype of any variable
+// console.log(typeof name);
+// console.log(name);
+
+// boolean
+let online = true;
+// console.log(typeof online);
+console.log(`Faraz is coding right now. Isn't it? ans: ${online}`);
