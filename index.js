8b6b49dc37dafbbec5232d76b07fe5a71c0e9868
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-11 01:12:38 +0600

    kivabe template literal er maddhome html website ae id dhore kono kichu print kora jay shikhano hoyeche

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-// 1.declaration let x;
-// 2. assignment x = 10;
+let fullName = 'Fardin Faraz';
+let age = 23;
+let is_student = true;
 
-//1 way
-// let x;
-// x = 120;
-
-// console.log(x);
-
-//2nd way
-// let x = 120;
-// console.log(x);
-
-//3rd 
-let age = 25;
-let price = 10.99;
-let gpa = 3.89;
-let name='Far';
-let lastname = 'az'
-// console.log(gpa);
-// console.log(`You are ${age} years old`);
-// console.log(`The price is $${price} in dollars`);
-// console.log(`Your gpa is ${gpa}`);
-// console.log(`Your name is ${name}${lastname}.`)
-
-// for seeing the datatype of any variable
-// console.log(typeof name);
-// console.log(name);
-
-// boolean
-let online = true;
-// console.log(typeof online);
-console.log(`Faraz is coding right now. Isn't it? ans: ${online}`);
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `My age is ${age}`;
+document.getElementById("p3").textContent = `I am a student. Isn't it true? ans is : ${age}`;
