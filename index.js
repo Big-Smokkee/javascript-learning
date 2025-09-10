aad6d571cabab4c90ee0a49bc8a469fbf8571274
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-11 01:27:24 +0600

    introduction to operators

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let fullName = 'Fardin Faraz';
-let age = 23;
-let is_student = true;
+// arithmetic operators =  operands (values, variables, etc.)
+//                         operators (+ - * /)
+//                         ex: 11 = x+5;
 
-document.getElementById("p1").textContent = `My name is ${fullName}`;
-document.getElementById("p2").textContent = `My age is ${age}`;
-document.getElementById("p3").textContent = `I am a student. Isn't it true? ans is : ${age}`;
+let a = 11;
+
+a = a+1;
+// a = a - 1;
+// a = a * 2;
+// a = a / 2;
+
+a = a ** 30;
+console.log(a);
