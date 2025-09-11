f698d8d9a39d4c8d5c288838b90607b9c77d7dbe
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-11 19:13:19 +0600

    getting input from user and showing it to the console.log

---
+++ index.js
@@ -0,0 +1,17 @@
+// How to accept user input
+
+// 1. Easy way = window prompt
+// 2. Professional way = HTML textbox
+
+
+//Easy way = window prompt
+// let username;
+// username = window.prompt(`what's you username?`);
+// document.getElementById('a').textContent = `You typed ${username}`
+
+// Professional way
+let username;
+document.getElementById("submit").onclick = function () {
+    username = document.getElementById("name").value;
+    console.log(username);
+}
