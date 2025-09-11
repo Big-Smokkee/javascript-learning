023e53df877ae15184581fb0da47493605192490
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-12 00:09:10 +0600

    Counter program using html css and js

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-//   const = "a variable that can't be changed"
+//count program
 
-// const PI = 3.14159;
-// let radius;
-// let circumferences;
+const decreaseBtn = document.getElementById("decrease-btn");
+const resetBtn = document.getElementById("reset-btn");
+const increaseBtn = document.getElementById("increase-btn");
+const countLabel = document.getElementById("count-label");
 
-// //what is I changed pi variable value
-// // pi = 9458490;
+let count = 0;
 
-// // so the best practice is using const pi.. After that we can't make any changes to it
-
-// // PI = 30.14159;
-// radius = window.prompt("enter the radius os a circle: ");
-// radius = Number(radius);
-// circumferences = 2 * PI * radius;
-
-// console.log(circumferences);
-
-
-
-// user website
-const PI = 3.14159;
-
-document.getElementById("submit-button").onclick = function () {
-    let radius = document.getElementById("radius-box").value;
-    radius = Number(radius);
-    let conference = 2 * PI *radius;
-    document.getElementById("ans").textContent = `The answer is : ${conference}.`;
-    // console.log(conference);
+increaseBtn.onclick = function () {
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function () {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function () {
+    count=0;
+    countLabel.textContent = count;
 }
