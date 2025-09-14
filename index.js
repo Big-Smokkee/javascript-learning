f1bd43e80a9e553c408a6e24581f45207402e2d8
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-14 22:37:11 +0600

    feat: logic of random number genarator

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-// Math - built in object that provides a collection of properties and methods.
-// console.log(Math.PI);
-// console.log(Math.E);
+// Random Number Genarator
+//my way
+let randomNum1 = Math.ceil(Math.random()* 6);
+console.log(randomNum1);
+// bro code way
+let randomNum2 = Math.floor(Math.random()* 6) + 1;
+console.log(randomNum2);
 
-let x = 4.21;
-let y = 2.99;
-let z;
+// random number between 51 and 100
+let rand3 = Math.ceil(Math.random() * 50) + 50;
+console.log(rand3);
 
-z = Math.round(x);
-console.log(Math.ceil(x));
-console.log(Math.floor(y));
-console.log(Math.trunc(y));
-console.log(z);
+// Bro code way
+const min = 50;
+const max = 100;
 
-console.log(Math.pow(Math.round(x), 2));
-z = Math.sqrt(Math.round(x));
-let zz = 10;
-zz = Math.log(zz);
-console.log(zz);
-
-let a = 45;
-let b = 0;
-let c = 60;
-a = Math.sin(a);
-b = Math.cos(b);
-c = Math.tan(c);
-console.log(Math.trunc(a));
-console.log(Math.trunc(b));
-console.log(Math.c);
-
-a = 78;
-console.log(Math.abs(a));
-console.log(Math.sign(a));
-
-let xa = 3;
-let ya = 1;
-let za = 5;
-
-let max = Math.max(xa, ya, za);
-let min = Math.min(xa, ya, za);
-console.log(max);
-console.log(min);
+let randBro = Math.floor(Math.random() * (max - min)) + min;
+console.log(randBro);
