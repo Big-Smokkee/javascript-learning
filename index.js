0b508261472ef8e0162b0bf039a021ecad8ec997
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-15 13:28:24 +0600

    Added: if else

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-// Random Number Genarator
-//my way
-let randomNum1 = Math.ceil(Math.random()* 6);
-console.log(randomNum1);
-// bro code way
-let randomNum2 = Math.floor(Math.random()* 6) + 1;
-console.log(randomNum2);
+// IF statements ==
+// If a condition is true, execute some code
+// if Notification, do something else
+//1st example
+// let age = 115;
+// if (age >= 18) {
+//     console.log("You are old enough to enter this site");
+// }
+// else
+// {
+//     console.log("You must be 18+ to enter this site");
+// }
+//2nd example
+// let time = 9;
+// if (time < 12)
+// {
+//     console.log("Good morning!");
+// }
+// else {
+//     console.log("Good afternoon!");
+    
+// }
+//3rd eample
+// let isStudent = true;
+// if (isStudent) {
+//     console.log("You are a student!");
+// }
+// else
+//     {
+//     console.log("You aren't a student!");
+// }
 
-// random number between 51 and 100
-let rand3 = Math.ceil(Math.random() * 50) + 50;
-console.log(rand3);
+//Nested if else
+///Order of the statements matter a lot when using else if
+let age = -15;
+let hasLicense = true;
 
-// Bro code way
-const min = 50;
-const max = 100;
-
-let randBro = Math.floor(Math.random() * (max - min)) + min;
-console.log(randBro);
+if (age >= 16) {
+    console.log("You are old enough to drive");
+    if (hasLicense) {
+        console.log("You have your license");
+    }
+    else
+        {
+        console.log("You dont have your license");
+    }
+}
+else if (age < 0)
+{
+    console.log("Are you even a human");
+}
+else
+{
+    console.log("You must be 16+ to have a license");
+}
