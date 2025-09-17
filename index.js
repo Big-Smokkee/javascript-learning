efef77ee662ef9fb0a2207982c0194307f5076f1
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-17 22:06:06 +0600

    switch case

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// let isStudent = true;
-// let result = isStudent ? "You are a student" : "You aren't a student";
-// console.log(result);
-let amount =25;
-let disc = amount >= 100 ? 10 : 0
-console.log(disc);
+let day = 1;
+
+switch (day) {
+    case 1:
+        console.log(`It is saturday`);
+        // break;
+    case 2:
+        console.log(`It is sunday`);
+        break;
+    case 3:
+        console.log(`It is monday`);
+        break;
+    case 4:
+        console.log(`It is tuesday`);
+        break;
+    case 5:
+        console.log(`It is wednesday`);
+        break;
+    case 6:
+        console.log(`It is thursday`);
+        break;
+    case 7:
+        console.log(`It is friday`);
+        break;
+    default:
+        console.log(`${day} isn't a day`);
+}
