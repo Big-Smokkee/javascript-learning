108142a5f2144683c066fe38fe6328cbbd72f27e
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-17 15:39:46 +0600

    cheked property

--- index.js
+++ index.js
@@ -1,55 +1,32 @@
-// IF statements ==
-// If a condition is true, execute some code
-// if Notification, do something else
-//1st example
-// let age = 115;
-// if (age >= 18) {
-//     console.log("You are old enough to enter this site");
-// }
-// else
-// {
-//     console.log("You must be 18+ to enter this site");
-// }
-//2nd example
-// let time = 9;
-// if (time < 12)
-// {
-//     console.log("Good morning!");
-// }
-// else {
-//     console.log("Good afternoon!");
+//  .checked = property that determines the checked state of an HTML checkbox or radio button element
+
+const checkbox = document.getElementById("myCheckBox");
+const visa = document.getElementById("visaBtn");
+const master = document.getElementById("masterCardBtn");
+const paypal = document.getElementById("paypalBtn");
+const submit = document.getElementById("submitBtn");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+submit.onclick = function () {
+    if (checkbox.checked) {
+        subResult.textContent = `${checkbox.checked} User has subscribed`;
+
+        if (visa.checked) {
+            paymentResult.textContent = `${visa.checked} user has subscribed through VISA`;
+        }
+        else if (master.checked) {
+            paymentResult.textContent = `${master.checked} user has subscribed through Master`;
+        }
+        else if (paypal.checked) {
+            paymentResult.textContent = `${paypal.checked} user has subscribed through PayPal`;
+        }
+        else {
+            paymentResult.textContent = `user may subscribed through Cash`;
+        }
+    }
+    else {
+        subResult.textContent = `${checkbox.checked} User hasn't subscribed`;  
+    }
     
-// }
-//3rd eample
-// let isStudent = true;
-// if (isStudent) {
-//     console.log("You are a student!");
-// }
-// else
-//     {
-//     console.log("You aren't a student!");
-// }
-
-//Nested if else
-///Order of the statements matter a lot when using else if
-let age = -15;
-let hasLicense = true;
-
-if (age >= 16) {
-    console.log("You are old enough to drive");
-    if (hasLicense) {
-        console.log("You have your license");
-    }
-    else
-        {
-        console.log("You dont have your license");
-    }
-}
-else if (age < 0)
-{
-    console.log("Are you even a human");
-}
-else
-{
-    console.log("You must be 16+ to have a license");
 }
