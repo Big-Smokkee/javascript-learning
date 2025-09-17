8c2c8efa6cbb46a96f4627ccfa7e059199afdf8f
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-17 21:35:17 +0600

    ternary again

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let isStudent = true;
-let result = isStudent ? "You are a student" : "You aren't a student";
-console.log(result);
+// let isStudent = true;
+// let result = isStudent ? "You are a student" : "You aren't a student";
+// console.log(result);
+let amount =25;
+let disc = amount >= 100 ? 10 : 0
+console.log(disc);
