f9f8a09ddfec461ad1f8845a60b4bf35bbbdb943
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-17 21:31:35 +0600

    ternary operation

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-//  .checked = property that determines the checked state of an HTML checkbox or radio button element
-
-const checkbox = document.getElementById("myCheckBox");
-const visa = document.getElementById("visaBtn");
-const master = document.getElementById("masterCardBtn");
-const paypal = document.getElementById("paypalBtn");
-const submit = document.getElementById("submitBtn");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
-
-submit.onclick = function () {
-    if (checkbox.checked) {
-        subResult.textContent = `${checkbox.checked} User has subscribed`;
-
-        if (visa.checked) {
-            paymentResult.textContent = `${visa.checked} user has subscribed through VISA`;
-        }
-        else if (master.checked) {
-            paymentResult.textContent = `${master.checked} user has subscribed through Master`;
-        }
-        else if (paypal.checked) {
-            paymentResult.textContent = `${paypal.checked} user has subscribed through PayPal`;
-        }
-        else {
-            paymentResult.textContent = `user may subscribed through Cash`;
-        }
-    }
-    else {
-        subResult.textContent = `${checkbox.checked} User hasn't subscribed`;  
-    }
-    
-}
+let isStudent = true;
+let result = isStudent ? "You are a student" : "You aren't a student";
+console.log(result);
