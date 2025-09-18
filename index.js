f9780dd61e8f5d7690eb0a97122ee51b449758b1
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-18 14:41:56 +0600

    logical operator

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-// Method chaining = calling one method after another in one continues line of code...
+// Logical operator = used to combine or manipulate boolean values (true or false)
+// mojar kotha hoilo eshb kichu c++ er motoi
+// And = && , OR = ||, NOT = !
+const temp = 1;
+if (temp <= 0 || temp >= 30) {
+    console.log("Weather isn't good");
+}
+else
+    {
+    console.log("The weather is good");
+}
 
-
-
-///Amar mone hoitese jinish ta c++ er continue er moto
-
-let username = window.prompt("Enter your User Name");
-// console.log(username);
-/*              No method chaining
-//1ta solution
-// username = username.toUpperCase();
-// username = username.replace('f', "F");
-// username = username.replace('f', "F");
-// console.log(username);
-
-//2nd
-// username = username.replaceAll('f', 'F');
-// console.log(username);
-
-
-//3rd (bro Code)
-let firstLetter = username.charAt(0);
-firstLetter = firstLetter.toUpperCase();
-
-let extraChars = username.slice(1);
-extraChars = extraChars.toLowerCase();
-// console.log(firstLetter);
-// console.log(extraChars);
-username = firstLetter + extraChars;
-console.log(username);
-// username = username.replaceAll(username.in, 'F');
-
-*/
-
-///Here comes method chaining
-
-// boom boom combos
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
-
-console.log(username);
-
-
-///Daruuun
+const isSunny = true;
+if (!isSunny) {
+    console.log("It is cloudy");
+}
+else {
+    console.log("It is sunny");
+}
