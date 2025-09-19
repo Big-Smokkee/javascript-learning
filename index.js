a6e9985b66ffbb98f30106d2f55637f6e8fb8525
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-09-19 21:45:45 +0600

    for loop in js

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-// while-looop
+//For loop
+for (let i = 1; i <= 3; i++)
+{
+    // console.log("Hello");
+    console.log(i);
+}
+//heda edi pari
 
-// let user = " ";
-// user = user.trim();
-// while (user === "" || user ===null)
-// {
-//     // console.log("You didn't enter anything");
-//     // break;
-//     user = prompt("Enter your name");
-// }
-// console.log(`hello: ${user}`);
-
-// do while loop
-
-// let user;
-// while (user === "" || user === null){
-//     user = prompt("Enter your name");
-// }
-
-// console.log(`Hello ${user}`);
-
-let loggedIn = false;
-let username;
-let pass;
-
-while (!loggedIn) {
-    username = prompt("Enter your user name");
-    pass = prompt("Enter your password");
-
-    if (username === "myUsername" && pass === "myPassword")
-    {
-        loggedIn = true;
-        console.log(`You are logged in`);
-    }
-    else
-    {
-        console.log(`Invalid creadential`);
-    }
+//continue
+for (let i = 1; i <= 20; i++)
+{
+    if (i == 13) continue;
+    console.log(i);
 }
